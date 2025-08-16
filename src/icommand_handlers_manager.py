5ecd397abaa7d229d5d507b549acdfe2841a7c77
Alexander Antoshkin <ighost99@gmail.com>
2025-08-17 00:14:43 +0200

    feat: add ICommandHandlersManager interface

---
+++ src/icommand_handlers_manager.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class ICommandHandlersManager(ABC):
+    """
+    Abstract interface for command handlers manager.
+    
+    This interface defines the contract for managing command handlers in the bot.
+    """
+    
+    @abstractmethod
+    def populate_bot_handlers(self) -> None:
+        """
+        Populate all command handlers in CommandHandlersRegistry.
+        """
+        pass
+    
+    @abstractmethod
+    def register(self) -> None:
+        """
+        Register all handlers in the bot.
+        """
+        pass
